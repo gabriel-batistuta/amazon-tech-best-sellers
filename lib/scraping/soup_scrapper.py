cdffafd9e6b081a9274147dfd4b0aa1e5c172e7f
gabriel <batistutag190@gmail.com>
2024-05-04 16:41:48 -0300

    initial implementation of soup scrapper

--- lib/scraping/soup_scrapper.py
+++ lib/scraping/soup_scrapper.py
@@ -1,2 +1,25 @@
 from requests import get
 from bs4 import BeautifulSoup
+from typing import Union
+
+class BS4_Web_Scraper:
+    def __init__(self) -> None:
+        pass
+
+    def get_page(self, url):
+        response = get(url)
+        return BeautifulSoup(response.content, 'html.parser').prettify()
+
+    def parse_page(self, page):
+        pass
+
+def parse_page(url):
+    soup = BS4_Web_Scraper()
+    page = soup.get_page(url)
+    res = parse_page(page)
+    return res
+
+if __name__ == '__main__':
+    soup = BS4_Web_Scraper()
+    url = 'https://www.amazon.com.br/gp/bestsellers/computers/'
+    page = soup.get_page(url)
